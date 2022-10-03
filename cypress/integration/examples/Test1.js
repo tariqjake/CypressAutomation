fd809996279c5da92530c5c064dbaa0dd78b16ca
Tarik <tarik@Tariks-iMac.lan>
2022-10-03 09:48:37 -0500

    created a new cypress project with configurations and example test case

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,13 @@
+//cypress - Spec
+describe('My First Test Suite', function(){
+
+    it('My First Test Case', function(){
+        //test step
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    })
+
+    it('My Second Test Case', function() {
+        
+    })
+
+})
